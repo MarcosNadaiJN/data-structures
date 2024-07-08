31575dcc5a7ba80a05a7dbf2ee0d5da7c975fbb1
Marcos Nadai Junior <marcos.junior@elotech.com.br>
2024-07-08 18:24:27 -0300

    adds comments

--- src/main/java/org/example/structures/MyLinkedList.java
+++ src/main/java/org/example/structures/MyLinkedList.java
@@ -52,7 +52,7 @@ public class MyLinkedList<T> {
 
     public void removeFromEnd(){
         Node<T> current = head;
-        for(int i = 1; i < length - 1; i++) {
+        for(int i = 1; i < length - 1; i++) { //it will stop at the last but one, so wee can set it "next" field as null, therefore this is the new last node;
             current = current.next;
         }
         current.next = null;
@@ -66,7 +66,7 @@ public class MyLinkedList<T> {
             return this.getLast();
         } else {
             Node<T> current = head;
-            for (int i = 1; i < position; i++) {
+            for (int i = 1; i < position; i++) { //it will stop one position before the one wee want, so we just need to return the "next" field;
                 current = current.next;
             }
             return current.next;
@@ -84,8 +84,8 @@ public class MyLinkedList<T> {
                 temp = temp.next;
             }
             Node<T> newNode = new Node<>(data);
-            newNode.next = temp.next;
-            temp.next = newNode;
+            newNode.next = temp.next; //att this point the "temp" node, is one position before the position wee want, so wee need to set its "next" field, with the value in the "next" field of the "temp" variable;
+            temp.next = newNode; // now wee need to set the "next" field of the "temp" node, as the "newNode". Now basically, the newNode is between the two nodes that already existed, in the position wee wanted;
             length++;
         }
     }
@@ -100,8 +100,8 @@ public class MyLinkedList<T> {
             for (int i = 1; i < position; i++) {
                 current = current.next;
             }
-            Node<T> nodeToBeRemoved = current.next;
-            current.next = nodeToBeRemoved.next;
+            Node<T> nodeToBeRemoved = current.next; //here we store the node that wee want to remove, that is currently in the "next" field of the "current node", so we can access some info later;
+            current.next = nodeToBeRemoved.next; //we remove the node, by setting the "next" field of the current node (the node before the one we want to remove) with the node stored in the "next" field of the node that we want to remove, we basically skip the node;
             nodeToBeRemoved.next = null;
             length--;
         }
